a42464e69c3e252f61dc9e330239a336f9156fcb
Philip <killfilipok@gmail.com>
2019-03-04 18:13:17 +0200

    lesson-03

---
+++ Smack/View/ViewMy.swift
@@ -0,0 +1,22 @@
+//
+//  ViewMy.swift
+//  Smack
+//
+//  Created by Philip on 3/4/19.
+//  Copyright © 2019 Philip. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class ViewMy: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
